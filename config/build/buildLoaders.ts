2d9f1fc6d9a5d736c88864bcdbfdc1db910fdc94
eanikonova <eanikonova@phoenixit.ru>
2023-10-06 17:57:12 +0300

    add devServer

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,13 +1,13 @@
 import webpack from 'webpack';
 
 export function buildLoaders(): webpack.RuleSetRule[] {
-    const typescriptLoader = {
+    const typeScriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
     };
 
     return [
-        typescriptLoader,
+        typeScriptLoader,
     ]
 }
--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,24 +1,26 @@
-import path from 'path';
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 import { buildPlugins } from './buildPlugins';
 import { buildLoaders } from './buildLoaders';
 import { buildResolvers } from './buildResolvers';
+import {buildDevServer} from './buildDevServer';
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const {mode, paths} = options;
+    const {mode, paths, isDev} = options;
     return {
         mode: mode,
-        entry: paths.html,
+        entry: paths.entry,
         output: {
-        filename: "[name].[contenthash].js",
-        path: paths.build,
-        clean: true,
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true,
         },
         plugins: buildPlugins(options),
         module: {
             rules: buildLoaders(),
         },
         resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
     }
 }
