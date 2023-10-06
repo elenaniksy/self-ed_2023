b60280a8c944ae8b28f56d166bd240987b2594d7
eanikonova <eanikonova@phoenixit.ru>
2023-10-06 19:37:45 +0300

    добавлена асинхронная подгрузка чанков при при сборке

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { Counter } from "./components/Counter"
+import { Suspense } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+
 import './index.scss';
+
 
 const App = () => {
     return (
         <div>
-            <Counter />
+            <Link to="/">Главная</Link>
+            <Link to="/about">О сайте</Link>
+
+            <Suspense fallback={<div>Loading...</ div>}>
+                <Routes>
+                    <Route path="/about" element={<AboutPageAsync />}/>
+                    <Route path="/" element={<MainPageAsync />}/>                   
+                </Routes>  
+            </Suspense>                   
         </div>
     )
 }
---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+
+    return (
+        <div>
+            ABOUT
+        </div>
+    )
+}
+
+export default AboutPage;
