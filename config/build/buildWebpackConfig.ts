53514c350f7b6360fb3a9d9dd36e77911491bb6b
eanikonova <eanikonova@phoenixit.ru>
2023-10-06 17:24:48 +0300

    add webpack decomposition

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
+import { buildPlugins } from './buildPlugins';
+import { buildLoaders } from './buildLoaders';
+import { buildResolvers } from './buildResolvers';
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const {mode, paths} = options;
+    return {
+        mode: mode,
+        entry: paths.html,
+        output: {
+        filename: "[name].[contenthash].js",
+        path: paths.build,
+        clean: true,
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(),
+        },
+        resolve: buildResolvers(),
+    }
+}
