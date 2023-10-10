4f39ebe29155f4b2386af5b382c10c1965e91986
eanikonova <eanikonova@phoenixit.ru>
2023-10-10 18:58:04 +0300

    add router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,11 @@
-import { Suspense } from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./providers/Router";
 import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
 import { classNames } from "shared/lib/classNames/classNames";
 
 import './styles/index.scss';
+
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -16,12 +15,7 @@ const App = () => {
             <Link to="/">Главная</Link>
             <Link to="/about">О сайте</Link>
 
-            <Suspense fallback={<div>Loading...</ div>}>
-                <Routes>
-                    <Route path="/about" element={<AboutPage />}/>
-                    <Route path="/" element={<MainPage />}/>                   
-                </Routes>  
-            </Suspense>                   
+            <AppRouter />               
         </div>
     )
 }
