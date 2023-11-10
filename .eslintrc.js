34a83959dfc386fe2e843ba9a81a7f2c24beb7c3
eanikonova <eanikonova@phoenixit.ru>
2023-11-10 20:55:10 +0300

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,7 +35,13 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'i18next/no-literal-string': [
+            'error',
+            {
+                markupOnly: true,
+                ignoreAttribute: ['data-test-id'],
+            },
+        ],
         'max-len': ['error', { ignoreComments: true }, { code: 100 }],
     },
     globals: {
